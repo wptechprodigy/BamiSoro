1c04358bbd4a0f27b9bb56f85121384bc3f7683c
Waheed Afolabi <waheed.freelancer16@gmail.com>
2020-11-07 14:11:01 +0100

    add login form

---
+++ BamiSoro/Constants/BamiSoroContants.swift
@@ -0,0 +1,27 @@
+//
+//  BamiSoroContants.swift
+//  BamiSoro
+//
+//  Created by waheedCodes on 07/11/2020.
+//
+
+import UIKit
+
+enum LoginConstant {
+    static let isLoggedIn = "isLoggedIn"
+    static let loginTitle = "Log In"
+}
+
+enum RegisterConstant {
+    static let register = "Register"
+    static let createAccount = "Create Account"
+}
+
+enum ImageAssetsConstant {
+    static let logo = "Logo"
+}
+
+enum PlaceholderConstant {
+    static let email = "Email Address..."
+    static let password = "Password..."
+}
