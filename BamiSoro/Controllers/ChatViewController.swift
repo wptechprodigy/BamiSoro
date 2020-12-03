aceaccfef3da61ded21bad5abb82493a77495e0c
Waheed Afolabi <waheed.freelancer16@gmail.com>
2020-12-03 19:54:31 +0100

    add sample chat functionality

--- BamiSoro/Controllers/ChatViewController.swift
+++ BamiSoro/Controllers/ChatViewController.swift
@@ -22,10 +22,44 @@ struct Sender: SenderType {
 }
 
 class ChatViewController: MessagesViewController {
+    
+    private var messages = [Message]()
+    
+    private let selfSender = Sender(photoURL: "",
+                                    senderId: "1",
+                                    displayName: "Joe Allen")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemRed
+        
+        messages.append(Message(sender: selfSender,
+                                messageId: "1",
+                                sentDate: Date(),
+                                kind: .text("Hello world message.")))
+        
+        messages.append(Message(sender: selfSender,
+                                messageId: "2",
+                                sentDate: Date(),
+                                kind: .text("This is the second message.")))
+        
+        messagesCollectionView.messagesDataSource = self
+        messagesCollectionView.messagesLayoutDelegate = self
+        messagesCollectionView.messagesDisplayDelegate = self
     }
 
+}
+
+extension ChatViewController: MessagesDataSource, MessagesLayoutDelegate, MessagesDisplayDelegate {
+    func currentSender() -> SenderType {
+        return selfSender
+    }
+    
+    func messageForItem(at indexPath: IndexPath, in messagesCollectionView: MessagesCollectionView) -> MessageType {
+        return messages[indexPath.section]
+    }
+    
+    func numberOfSections(in messagesCollectionView: MessagesCollectionView) -> Int {
+        return messages.count
+    }
+    
 }
